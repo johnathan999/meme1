18e20f5fea48894fe5ce6302172ce8d1989e65b5
johnathan999 <45374336+johnathan999@users.noreply.github.com>
2019-02-20 00:53:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const low = require("lowdb");
 const FileSync = require('lowdb/adapters/FileSync');
 const settings = require("./settings.json");
 const DBL = require("dblapi.js");
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ1MDQ0OTQzMzM0NDM0NDA2NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTM1OTMzNjQwfQ.7lcFDOm30a-d-GhCt2rULUvz4eZn-ZBQCg4SgXA6SI8', client);
+const dbl = new DBL(process.env.DBL, client);
 const Client = new Discord.Client();
 
 
@@ -14,7 +14,7 @@ const db = low(adapter);
 
 db.defaults({ histoires: [], xp: []}).write();
 var prefix = "ub!!";
-client.login("NDUwNDQ5NDMzMzQ0MzQ0MDY0.D04y7w.FjWS0l-yeCjqKt03wEA5pXOXA18");
+client.login(process.env.TOKEN);
 
 // Optional events
 dbl.on('posted', () => {
